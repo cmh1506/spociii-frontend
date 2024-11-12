b7071fa9b76a5ae6410d099cb93852e089625d83
cmh1506 <cmh1506@yahoo.de>
2024-11-12 21:04:06 +0100

    some commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //path: "http://code4u.cloud:3000"
-  path: "http://localhost:3000"
+  path: "http://code4u.cloud:3000"
+  //path: "http://localhost:3000"
 }
