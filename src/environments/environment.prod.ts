b2c6838a29ea31235f306116c2862a5706e94383
cmh1506 <cmh1506@yahoo.de>
2024-12-30 22:00:34 +0100

    Optik

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   //path: "http://code4u.cloud:3000"
-  //path: "http://87.106.151.170:3000"
-  path: "http://localhost:3000"
+  path: "http://87.106.151.170:3000" //code4u2.cloud
+  //path: "http://localhost:3000"
   //path: "http://217.160.79.116:3000" //
   
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  path: "http://localhost:3000"  
-  //path: "http://87.106.151.170:3000"
+  //path: "http://localhost:3000"  
+  path: "http://87.106.151.170:3000" //code4u2.cloud
   //path: "http://code4u.cloud:3000"
 }
